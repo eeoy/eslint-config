5ffaa02ff01c2e1a6840af34001547edbc94d898
mmazzz <mmazzz123@gmail.com>
2021-03-06 05:39:51 +0900

    fix(typescript): corerct order for extending config

--- packages/eslint-config-typescript/lib/index.js
+++ packages/eslint-config-typescript/lib/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: [
-    require.resolve('@eeoy/eslint-config-typescript/lib/import'),
     require.resolve('@eeoy/eslint-config'),
+    require.resolve('@eeoy/eslint-config-typescript/lib/import'),
     require.resolve('@eeoy/eslint-config-typescript/lib/typescript'),
     require.resolve('@eeoy/eslint-config-typescript/lib/extension'),
   ],
